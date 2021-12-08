6e684259ba53e03539c8dd398e29fd8c83835af1
Phong Ho <phongho@Phongs-MacBook-Pro.local>
2021-12-08 18:29:37 +0700

    slight modifications

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -16,7 +17,8 @@ mongoose
     .catch((err) => {
         console.log(err);
     });
-
+    
+app.use(cors())
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
